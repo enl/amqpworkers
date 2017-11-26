739f5790de7c529ba906f5b2f20cd63fcb964b58
Alex Panshin <deadyaga@gmail.com>
2017-11-26 21:03:32 +0200

    #4 add batch size limit parameter

--- src/BatchProducer.php
+++ src/BatchProducer.php
@@ -2,6 +2,8 @@
 
 namespace AmqpWorkers;
 
+use PhpAmqpLib\Connection\AbstractConnection;
+
 /**
  * BatchProducer treats given message as a list of messages and send them to RabbitMQ channel
  *
@@ -11,6 +13,16 @@ namespace AmqpWorkers;
  */
 class BatchProducer extends Producer
 {
+    private $limit = 0;
+    private $queued = 0;
+
+    public static function factory(AbstractConnection $connection, $limit = 100)
+    {
+        /** @var BatchProducer $producer */
+        $producer = parent::factory($connection);
+        $producer->limit = $limit;
+    }
+
     /**
      * @param array|\Traversable $messages
      * @throws \AmqpWorkers\Exception\ProducerNotProperlyConfigured
@@ -18,15 +30,26 @@ class BatchProducer extends Producer
     public function produce($messages)
     {
         $channel = $this->getChannel();
-
         foreach ($messages as $payload) {
             if ($this->isExchange()) {
                 $channel->batch_basic_publish($this->createMessage($payload), $this->exchange->getName());
             } else {
                 $channel->batch_basic_publish($this->createMessage($payload), '', $this->queue->getName());
             }
+
+            $this->queued++;
+
+            if ($this->limit && $this->limit > $this->queued) {
+                $this->flush();
+            }
         }
 
-        $channel->publish_batch();
+        $this->flush();
+    }
+
+    protected function flush()
+    {
+        $this->getChannel()->publish_batch();
+        $this->queued = 0;
     }
 }
